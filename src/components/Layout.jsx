9a91ebd4e055e5ebd3e94e220120371704a748df
Mohammad <mohammad.mhbn@gmail.com>
2025-08-29 00:34:29 +0330

    upload project

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+
+function Layout() {
+  return (
+    <div>
+      <AppNav />
+      <Outlet />
+      <footer style={{ marginTop: "5rem" }}>this is footer section</footer>
+    </div>
+  );
+}
+export default Layout;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const [isAuth, setIsAuth] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/app/dashboard", { replace: true, state: "this is dummy data" });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {isAuth && <Navigate to="/app/dashboard" replace={true} />}
+      <form onSubmit={handleSubmit}>
+        <button>Login</button>
+      </form>
+      <button onClick={() => setIsAuth(!isAuth)}>Toggle Auth</button>
+    </div>
+  );
+}
+export default Login;
+
